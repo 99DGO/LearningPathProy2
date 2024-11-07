eb75d87682446162a38f44c5bbb9dd65637a1cc4
99DGO <d.gonzalezo2@uniandes.edu.co>
2024-11-06 19:33:52 -0500

    first

---
+++ LearningPathProy2/src/datosEstudiantes/DatosEstudianteEncuesta.java
@@ -0,0 +1,28 @@
+package datosEstudiantes;
+
+import java.util.Date;
+
+import envios.EnvioEncuesta;
+
+public class DatosEstudianteEncuesta extends DatosEstudianteActividad {
+	
+	private EnvioEncuesta envio;
+	
+	public DatosEstudianteEncuesta(String loginEstudiante) {
+		super(loginEstudiante);
+	}
+	
+	
+	public DatosEstudianteEncuesta(String loginEstudiante, String estado, Date fechaInicio, Date fechaFinal) {
+		super(loginEstudiante, estado, fechaInicio, fechaFinal);
+	}
+
+
+
+	public void finalizarEncuesta() throws Exception {
+		finalizarActividad();
+		setEstado(DatosEstudianteActividad.EXITOSO);
+	}
+	
+
+}
---
+++ LearningPathProy2/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module LearningPathProy2 {
+}

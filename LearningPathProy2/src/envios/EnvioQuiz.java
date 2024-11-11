1a9e2bb1928a6bbbbc586e982a58b9cc83939d51
5haiel <sm.jimenezp1@uniandes.edu.co>
2024-11-10 23:51:20 -0500

    Modified EnvioQuiz & EditorQuiz

--- LearningPathProy2/src/envios/EnvioQuiz.java
+++ LearningPathProy2/src/envios/EnvioQuiz.java
@@ -9,46 +9,48 @@ import caminosActividades.Quiz;
 
 public class EnvioQuiz extends Envio<PreguntaQuiz> {
 	private double calificacion;
-	
+
 	public EnvioQuiz() {
 		super();
 	}
-	
+
 	public EnvioQuiz(HashMap<PreguntaQuiz, String> respuestas, double calificacion) {
 		super(respuestas);
 		this.calificacion = calificacion;
 	}
 
 	public void agregarRespuesta(PreguntaQuiz pregunta, String respuesta) {
-          respuestas.put(pregunta, respuesta);
-    }
-	
+		respuestas.put(pregunta, respuesta);
+	}
+
 	public double getCalificacion() {
 		return calificacion;
 	}
+
 	public void setCalificacion(double calificacion) {
 		this.calificacion = calificacion;
 	}
+
 	public HashMap<PreguntaQuiz, String> getRespuestas() {
 		return respuestas;
 	}
-	
+
 	public double calcularCalificacionQuiz(Quiz quiz) {
-        List<PreguntaQuiz> preguntasQuiz = quiz.getPreguntas();
-        int totalPreguntas = preguntasQuiz.size();
-        int respuestasCorrectas = 0;
+		List<PreguntaQuiz> preguntasQuiz = quiz.getPreguntas();
+		int totalPreguntas = preguntasQuiz.size();
+		int respuestasCorrectas = 0;
 
-        for (PreguntaQuiz pregunta : preguntasQuiz) {
-            OpcionQuiz opcionCorrecta = pregunta.getRespuesta();
-            String respuestaCorrecta = opcionCorrecta.getTexto();
-            String respuestaUsuario = respuestas.get(pregunta);
+		for (PreguntaQuiz pregunta : preguntasQuiz) {
+			OpcionQuiz opcionCorrecta = pregunta.getRespuesta();
+			String respuestaCorrecta = opcionCorrecta.getTexto();
+			String respuestaUsuario = respuestas.get(pregunta);
 
-            if (respuestaUsuario != null && respuestaUsuario.equals(respuestaCorrecta)) {
-                respuestasCorrectas++;
-            }
-        }
-        
-        return (respuestasCorrectas / (double) totalPreguntas) * 10; //Calificacion del 1 al 10
-    }
+			if (respuestaUsuario != null && respuestaUsuario.equals(respuestaCorrecta)) {
+				respuestasCorrectas++;
+			}
+		}
+
+		return (respuestasCorrectas / (double) totalPreguntas) * quiz.getCalificacionMin(); // Calificacion dada por el profesor
+	}
 
 }

5676dd5c45d03f31ee96aba2f40a1752f33aa779
99DGO <d.gonzalezo2@uniandes.edu.co>
2024-11-11 18:38:57 -0500

    first commit

--- LearningPathProy2/src/persistencia/EstudiantesPersistencia.java
+++ LearningPathProy2/src/persistencia/EstudiantesPersistencia.java
@@ -1,11 +1,39 @@
 package persistencia;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.JSONException;
+
 import usuarios.Estudiante;
 
 public class EstudiantesPersistencia {
 	
-	public static void guardarEstudianteSingular(Estudiante estudiante, String pathEstudiantes)
+	public static void guardarEstudianteSingular(Estudiante estudiante, String pathEstudiantes, String pathCarpetaEstudiante)
 	{
+		//Creo el archivo con la información del camino
+		FileWriter fileCamino;
+		try 
+		{
+			fileCamino = new FileWriter(pathCarpetaCamino+"/"+caminoID+".json");
+			
+			try 
+			{
+				fileCamino.write(jCamino.toString(1));
+			} 
+			catch (JSONException e) 
+			{
+				e.printStackTrace();
+			}
+			
+			fileCamino.flush();
+			fileCamino.close();
+			
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 		
 	}
 

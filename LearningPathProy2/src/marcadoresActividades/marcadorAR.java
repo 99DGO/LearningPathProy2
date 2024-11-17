f2f68d28ee95fdd9a8ac4f72f1a5bb59ba8deab9
99DGO <d.gonzalezo2@uniandes.edu.co>
2024-11-16 19:03:31 -0500

    marcadorARTest

--- LearningPathProy2/src/marcadoresActividades/marcadorAR.java
+++ LearningPathProy2/src/marcadoresActividades/marcadorAR.java
@@ -37,6 +37,10 @@ public class marcadorAR
 		{
 			throw new Exception ("No existe una actividad con ese id");
 		}
+		else if (!actividad.getType().equals(Actividad.ACTIVIDADRECURSO))
+		{
+			throw new Exception ("La actividad pasada no fue una actividad de recurso.");
+		}
 		
 		//Tira exception de que no se encontro el dato del estudiante
 		DatosEstudianteAR datosEstudiante = (DatosEstudianteAR) actividad.getDatoEstudianteIndFromIDEstudiante(idEstudiante);
